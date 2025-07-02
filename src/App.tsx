e0aaa09b02add028b10dfe3507b024b6c332922a
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-07-02 20:52:18 +0700

    Implement scoring for selected dice

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ type Die = {
   selected: boolean;
 };
 type Dice = Die[];
-type TargetScore = number;
+type GoalScore = number;
 type TotalScore = {
   player: number;
   opponent: number;
@@ -22,44 +22,104 @@ type SelectedScore = {
   opponent: number;
 };
 
-function App() {
-  function initializeDice() {
-    return [
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-      {
-        value: Math.floor(Math.random() * 6) + 1,
-        kept: false,
-        selected: false,
-      },
-    ];
+function generateDie() {
+  return {
+    value: Math.floor(Math.random() * 6) + 1,
+    kept: false,
+    selected: false,
+  };
+}
+function initStarterDice() {
+  return [
+    generateDie(),
+    generateDie(),
+    generateDie(),
+    generateDie(),
+    generateDie(),
+    generateDie(),
+  ];
+}
+function calculateScore(dice: Die[]) {
+  function countSelectedDiceByValue(dice: Die[], val: number) {
+    return dice.filter((die) => die.selected && die.value === val).length;
   }
 
-  const [dice, setDice] = useState<Dice>(initializeDice);
-  const [targetScore, setTargetScore] = useState<TargetScore>(1500);
+  const numSelectedOnes = countSelectedDiceByValue(dice, 1);
+  const numSelectedTwos = countSelectedDiceByValue(dice, 2);
+  const numSelectedThrees = countSelectedDiceByValue(dice, 3);
+  const numSelectedFours = countSelectedDiceByValue(dice, 4);
+  const numSelectedFives = countSelectedDiceByValue(dice, 5);
+  const numSelectedSixes = countSelectedDiceByValue(dice, 6);
+
+  let score: number = 0;
+  if (numSelectedOnes === 1) {
+    score += 100;
+  } else if (numSelectedOnes === 2) {
+    score += 200;
+  } else if (numSelectedOnes === 3) {
+    score += 1000;
+  } else if (numSelectedOnes === 4) {
+    score += 1000 * 2;
+  } else if (numSelectedOnes === 5) {
+    score += 2000 * 2;
+  } else if (numSelectedOnes === 6) {
+    score += 4000 * 2;
+  }
+  if (numSelectedTwos === 3) {
+    score += 200;
+  } else if (numSelectedTwos === 4) {
+    score += 200 * 2;
+  } else if (numSelectedTwos === 5) {
+    score += 400 * 2;
+  } else if (numSelectedTwos === 6) {
+    score += 800 * 2;
+  }
+  if (numSelectedThrees === 3) {
+    score += 300;
+  } else if (numSelectedThrees === 4) {
+    score += 300 * 2;
+  } else if (numSelectedThrees === 5) {
+    score += 600 * 2;
+  } else if (numSelectedThrees === 6) {
+    score += 1200 * 2;
+  }
+  if (numSelectedFours === 3) {
+    score += 400;
+  } else if (numSelectedFours === 4) {
+    score += 400 * 2;
+  } else if (numSelectedFours === 5) {
+    score += 800 * 2;
+  } else if (numSelectedFours === 6) {
+    score += 1600 * 2;
+  }
+  if (numSelectedFives === 1) {
+    score += 50;
+  } else if (numSelectedFives === 2) {
+    score += 100;
+  } else if (numSelectedFives === 3) {
+    score += 500;
+  } else if (numSelectedFives === 4) {
+    score += 500 * 2;
+  } else if (numSelectedFives === 5) {
+    score += 1000 * 2;
+  } else if (numSelectedFives === 6) {
+    score += 2000 * 2;
+  }
+  if (numSelectedSixes === 3) {
+    score += 600;
+  } else if (numSelectedSixes === 4) {
+    score += 600 * 2;
+  } else if (numSelectedSixes === 5) {
+    score += 1200 * 2;
+  } else if (numSelectedSixes === 6) {
+    score += 2400 * 2;
+  }
+  return score;
+}
+
+function App() {
+  const [dice, setDice] = useState<Dice>(initStarterDice);
+  const [goalScore, setGoalScore] = useState<GoalScore>(1500);
   const [totalScore, setTotalScore] = useState<TotalScore>({
     player: 0,
     opponent: 0,
@@ -73,16 +133,6 @@ function App() {
     opponent: 0,
   });
 
-  function selectDie(dieIndex: number) {
-    setDice((prevDice) =>
-      prevDice?.map((die, i) =>
-        dieIndex === i
-          ? { ...die, kept: !die.kept, selected: !die.selected }
-          : die
-      )
-    );
-  }
-
   function rollDice() {
     setDice((prevDice) =>
       prevDice.map((die) =>
@@ -92,12 +142,24 @@ function App() {
       )
     );
   }
+  function handleDieClick(dieIdx: number) {
+    setDice((prevDice) => {
+      const newDice = prevDice.map((die, i) =>
+        dieIdx === i
+          ? { ...die, kept: !die.kept, selected: !die.selected }
+          : die
+      );
 
-  /* TODO
-  function keepAndRoll() {}
-  function keepAndPass() {}
-  function passRound() {}
-  */
+      const score = calculateScore(newDice);
+      setSelectedScore((prevSelectedScore) => {
+        return {
+          ...prevSelectedScore,
+          player: score,
+        };
+      });
+      return newDice;
+    });
+  }
 
   return (
     <div className="wrapper">
@@ -116,7 +178,7 @@ function App() {
 
           <section className="score">
             <h2>Goal</h2>
-            <p>{targetScore}</p>
+            <p>{goalScore}</p>
             <p>Round</p>
             <p>Selected</p>
           </section>
@@ -131,10 +193,10 @@ function App() {
 
         <section className="table">
           <ul className="dice">
-            {dice.map((die, index) => (
+            {dice.map((die, idx) => (
               <li
-                key={index}
-                onClick={() => selectDie(index)}
+                key={idx}
+                onClick={() => handleDieClick(idx)}
                 className={`die ${die.selected ? "selected" : ""}`}
               >
                 <DieIcon value={die.value} />
@@ -144,8 +206,8 @@ function App() {
         </section>
 
         <section className="actions">
-          <button type="button">Keep & roll</button>
-          <button type="button">Keep & pass</button>
+          <button type="button">Score and continue</button>
+          <button type="button">Score and pass</button>
           <button type="button">Pass</button>
           <button type="button" onClick={rollDice}>
             Roll (test)
